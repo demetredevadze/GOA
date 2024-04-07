cb1ebaf29bc739a3b10a870ee23701c6e1af6c2c
demetredevadze <demetre838@gmail.com>
2024-04-07 13:41:44 +0400

    commit

---
+++ day 03/homework/errors.py
@@ -0,0 +1,9 @@
+print("difikulte")
+
+difikulte = "go" mams ma a ma aam sams ams am am asamsas, xmsma sma mas ams amsamsa amsa ama samsa samsa, asma ams asm mas    # eror
+
+
+ # ეს ასე გაკეტდება ერორების გარეშე    
+
+ # difikulte = "go"
+ # print("difikulte")
