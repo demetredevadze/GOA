94ae479a8f3962fd1e41c03ff09a9b75e2b6b9a2
demetredevadze <demetre838@gmail.com>
2024-04-26 17:18:27 +0400

    commit

---
+++ day 20/homrwwork/hommework.py
@@ -0,0 +1,6 @@
+# შექმენით ცვლადი სახელად temperature და მიეცით ინტეჯერ ტიპის მნიშვნელობა, 1დან 100მდე რიცხვი. შემდეგ კი შეადარეთ სხვადასხვა რიცხვებს და გამოიტანეთ ეკრანზე. 
+temperature = int(61)
+temperatura = int(27)
+
+print(temperature < temperatura)
+print(temperature >  temperatura)
