9b48e73af4ae879b517ded5fdd9d59b8029f265c
demetredevadze <demetre838@gmail.com>
2024-05-25 21:11:48 +0400

    commit

---
+++ day 32/homework/hom.py
@@ -0,0 +1,11 @@
+#  შექმენით სია სადაც შეიყვანთ თქვენი ოჯახის წევრების სახელებს, შემდეგ კი დაბეჭდეთ თითოეული მათგანი ცალცალკე
+
+family = ["gvantsa", "ramazi", "nitsa", "demetre"]
+
+print(family[0])
+
+print(family[1])
+
+print(family[2])
+
+print(family[3])
