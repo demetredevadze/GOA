1f115f6f37c1a6ac0cb6054b6e28da53f7b5e559
demetredevadze <demetre838@gmail.com>
2024-10-13 15:58:01 +0400

    commit

---
+++ day 68/homework/hm.py
@@ -0,0 +1,53 @@
+# 1) შექმენით ფუნქცია რომელიც მიიღებს ადამიანის სახელს და შემდეგ დაუპრინტავს მისასალმებელ წერილს
+# 2) შექმენით ფუნქცია რომელსაც გადაეცემა რიცხვების სია შემდეგ გამოიტანეთ ამ ყველა რიცხვის ჯამი
+# 3) შექმენით ფუნქცია რომელსაც გადაეცემა სახელი და შემდეგ დაუბეჭდეთ რამდენი ასოა ამ სახელში 
+# 4) შექმენი ფუნქცია, რომელიც შემოატანინებს მომხარებელს ორ რიცხვს და დააბრუნებს მათ ჯამს.
+
+# 1
+
+# nam = input("enter your name: ")
+
+# def name(d):
+#     return "helo " + d
+
+# print(name(nam))
+
+# 2
+
+# my_list = [5, 8, 10000, 309, 9]
+
+# def list_sum(list):
+#     sum_of_list = 0
+#     for i in range(len(list)):
+#         sum_of_list = sum_of_list + list[i]
+
+#     print(sum_of_list)
+
+# list_sum(my_list)
+# 3
+
+# name = input("enter name -> ")
+
+
+# def number(num):
+#     for i in range(len(num)):
+#         print(num[i])
+
+        
+        
+
+
+
+# number(name)
+
+# 4
+
+# num = int(input("enter number -> "))
+# nam = int(input("enter number 2 -> "))
+
+# def sum(number , n):
+#     print(number + n)
+
+
+
+# sum(num , nam)
---
+++ day 69/homework/hm.py
@@ -0,0 +1,58 @@
+# 1) შექმენით ფუნქცია რომელიც მიიღებს ადამიანის სახელს და შემდეგ დაუპრინტავს მისასალმებელ წერილს
+# 2) შექმენით ფუნქცია რომელსაც გადაეცემა რიცხვების სია შემდეგ გამოიტანეთ ამ ყველა რიცხვის ჯამი
+# 3) შექმენით ფუნქცია რომელსაც გადაეცემა სახელი და შემდეგ დაუბეჭდეთ რამდენი ასოა ამ სახელში
+# 4) შექმენი ფუნქცია, რომელიც შემოატანინებს მომხარებელს ორ რიცხვს და დააბრუნებს მათ ჯამს.
+
+# 5) შექმენი ფუნქცია, რომელიც შემოატანინებს მომხარებელს და თუ ის დადებითი იქნება, დააბრუნებს "დადებითი", და თუ უარყოფითი იქნება, "უარყოფითი". 
+
+# 1
+
+# nam = input("enter your name: ")
+
+# def name(d):
+#     return "helo " + d
+
+# print(name(nam))
+
+# 2
+
+# my_list = [5, 8, 10000, 309, 9]
+
+# def list_sum(list):
+#     sum_of_list = 0
+#     for i in range(len(list)):
+#         sum_of_list = sum_of_list + list[i]
+
+# 3
+
+# name = input("enter name -> ")
+
+
+# def number(num):
+#     for i in range(len(num)):
+#         print(num[i])
+
+# 4
+
+# num = int(input("enter number -> "))
+# nam = int(input("enter number 2 -> "))
+
+# def sum(number , n):
+#     print(number + n)
+
+
+
+# sum(num , nam)
+
+# 5
+
+# name_1 = input("enter name -> ")
+# nam_2 = "demetre"
+# def name(num,name):
+#     if num == name:
+#         print("დადებითია")
+
+#     else:
+#         print("უარყოფითი")
+
+# name(name_1,nam_2)
