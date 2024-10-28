bcbaa41e5014a8b270dc1d3768c45222f58b3fdd
demetredevadze <demetre838@gmail.com>
2024-10-28 20:10:25 +0400

    commit

---
+++ day 74/homework/hm.py
@@ -0,0 +1,33 @@
+ 
+# 1) გადახედეთ დოკუმეტაციას - https://www.w3schools.com/python/python_functions.asp
+# 2)შექმენით ფუნქცია რომელიც მიიღებს ორ არგუმენტს, პირველს ციფრს, ხოლო მეორეს მომხმარებლის მიერ შეყვანილ ციფრს, შემდეგ დაბეჭდეთ ამ ორი არგუმენტის ნამრავლი.
+# 3)შექმენით ფუნქცია რომელიც მიიღებს ერთ რიცხვს და სიას და იპოვის ამ სიაში მე-x რიცხვს (ასეთ ავხსენი გაკვეთილზე)
+# W3Schools offers free online tutorials, references and exercises in all the major languages of the web. Covering popular subjects like HTML, CSS, JavaScript, Python, SQL, Java, and many, many more.
+# Image 
+
+# 1
+
+nu = 8
+num = int(input("int(input(enter number one -> "))
+
+def number(n,m):
+    print(n * m)
+
+number(nu, num)
+
+# 2
+
+num = int(input("enter number 0 >  <27 -> "))
+nums = [3,433,43,4,3,3434,4,5,7,9,765,4,32,3456,98,765,43,4567,8,76,543,5,68,765,43,456,7,654]
+
+def index(namber, numbers):
+    print(numbers[namber])
+
+
+index(num, nums)
+
+
+
+
+
+    
