d891794feb377d1aa79c4bd7d1268bdcd860831f
demetredevadze <demetre838@gmail.com>
2025-02-15 18:22:22 +0400

    commit

---
+++ day 57/klasswork/kl.py
@@ -0,0 +1,11 @@
+# day 57:
+# 1)შექმენით ფუნქცია, რომელშიც იქნება რიცხვებით სავსე სია, ფუნქციამ უნდა გამოითვალოს ამ სიაში არსებული რიცხვების ჯამი] 
+
+number_one = int(input("enter number: "))
+number_two = int(input("enter number two: "))
+
+def plast():
+    print(number_one + number_two)
+
+
+plast()
