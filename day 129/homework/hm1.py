f29a96ff316adbea6608ba518a5ad4eea5483d20
demetredevadze <demetre838@gmail.com>
2025-05-05 13:11:43 +0400

    commit

---
+++ day 129/homework/hm1.py
@@ -0,0 +1,25 @@
+#1) გააკეთეთ .split() მეთოდის 5 მაგალითი;
+#1
+list = "demetre"
+s = list.split()
+print(s)
+
+#2
+ex = "apple,orange,banana,grape"
+fruits = ex.split(',')
+print(fruits)
+
+#3
+te = "hello gays"
+ds = te.split()
+print(ds)
+
+#4
+s = "hello broo"
+d = s.split()
+print(d)
+
+#5
+num = "123-456-789"
+nums = num.split('-')
+print(nums)
---
+++ day 129/homework/hm2.py
@@ -0,0 +1,16 @@
+#2) გააკეთეთ .join() მეთოდის 5 მაგალითი;
+
+#1
+s = ['apple', 'banana', 'cherry']
+lt = ' & '.join(s)
+print(lt)
+
+#2
+names = ['demetre', ' ', 'merabi', ' ', 'mate']
+restart = ''.join(names)
+print(restart)
+
+#3
+gs = ['goa', ' ', 'school', ' ',  'math']
+r = ''.join(gs)
+print(r)
