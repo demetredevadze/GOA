399bcc3d4563803cb21dc8f9d4c3b4663869295d
demetredevadze <demetre838@gmail.com>
2025-07-23 22:29:19 +0400

    commit

---
+++ day 054/homework/.py
@@ -0,0 +1,13 @@
+# შექმენით პროგრამა რომელშიც გექნებათ ხილის სია კალათა (list), შემდეგ მომხარებელს შემოატანინეთ თავისი საყვარელი ხილი (input), for ციკლის საშვალებით შეამოწმეთ არის თუ არა ეს ხილი სიაში პირობითი განცხადების საშვალებით (if-else), თუ არის დაუბეჭდეთ "Good choice" თუ არ არის "Fruit not in basket"
+
+basket = ["ვაშლი" , "მსხალი" , "ატამი" , "ბანანი"]
+
+faworirt_fruet = input("enter your faworite fruet: ")
+
+for basket in faworirt_fruet:
+    if faworirt_fruet == basket:
+        print("good choice")
+    else:
+        print("Fruit not in basket")
+
+
---
+++ day 054/klasswork/kl.py
@@ -0,0 +1,8 @@
+list = ["ვაშლი", "ბანანი", "ატამი", "მსხალი"]
+input = input("დაწერე შენი საყვარელი ხილი: ")
+
+for i in list:
+    if i == input:
+        print("true")
+    else:
+        print("fals")
