ea254600ccb0e22d1ec6142976264c7bce07785c
demetredevadze <demetre838@gmail.com>
2025-07-23 22:28:33 +0400

    commit

--- day 054/klasswork/kl.py
+++
@@ -1,8 +0,0 @@
-list = ["ვაშლი", "ბანანი", "ატამი", "მსხალი"]
-input = input("დაწერე შენი საყვარელი ხილი: ")
-
-for i in list:
-    if i == input:
-        print("true")
-    else:
-        print("fals")
