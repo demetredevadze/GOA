35fdcfeec255ff69507c7241f34561045f7859a0
demetredevadze <demetre838@gmail.com>
2025-08-10 10:09:54 +0300

    commit

---
+++ day 150/homework/hm.js
@@ -0,0 +1,6 @@
+// მომხმარებელს შემოატანინეთ ორი რიცხვი და შეასრულეთ ყველა მოქმედება. (+, -, *, /, **, % ) და alert-ში გამოიტანეთ შედეგი ასეთი სახით. 'ჯამი არის: {ჯამი}'
+
+let Number1 = prompt('enter number: ')
+let number2 = prompt('enter number: ')
+let sum = prompt('enter syambol: ')
+console.log(number('Number1 sum number2'))
---
+++ day 155/klasswork/kl2.js
@@ -0,0 +1,7 @@
+// მომხმარებელს შემოატანინეთ რიცხვი და 0-დან ამ რიცხვის ჩათვლით გამოიტანეთ სამის გამოტოვებით ყველა რიცხვი
+
+let nu = number(prompt('enter number: '))
+
+for (let num = 0;  num < nu ; num += 3){
+    console.log(num)
+}
